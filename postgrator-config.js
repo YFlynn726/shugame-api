b8e86e2b2191caada5c9ecf413cb4abced96314e
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-05-04 12:23:16 -0400

    corrected scripts

--- postgrator-config.js
+++ postgrator-config.js
@@ -12,10 +12,5 @@ module.exports = {
     process.env.NODE_ENV === "test"
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
+  ssl: !!process.env.SSL,
 };
-
-// connectionString:
-//     process.env.NODE_ENV === "test"
-//       ? process.env.TEST_DATABASE_URL
-//       : process.env.DATABASE_URL,
-//   ssl: !!process.env.SSL,
